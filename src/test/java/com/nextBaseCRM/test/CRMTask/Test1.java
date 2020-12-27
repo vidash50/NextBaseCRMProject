62ef968fe4860565d7fd07689bd047c419f82df1
Vida Duodu <vidash50@gmail.com>
2020-12-26 23:46:25 -0500

    initial commit

---
+++ src/test/java/com/nextBaseCRM/test/CRMTask/Test1.java
@@ -0,0 +1,31 @@
+package com.nextBaseCRM.test.CRMTask;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http:/nextbasecrm.com");
+
+        String actualTitle = "login.nextbasecrm";
+        String expectedTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("nextBaseCRM verification Passed");
+        }else {
+            System.out.println("nextBaseCRM verification Passed");
+        }
+
+
+
+
+
+    }
+}
